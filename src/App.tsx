5832430e821e20c03f51131f3341da648dc8d5e1
levabala <levabala@ya.ru>
2021-11-20 11:09:18 +0300

    feat: now it's starting up

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import type { Component } from "solid-js";
 import styles from "./App.module.scss";
 
 const App: Component = () => {
-  return <div class={styles.App}></div>;
+  return <div class={styles.App}>budjet</div>;
 };
 
 export default App;
